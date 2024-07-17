fefe6dd6a8eed0a4ee10888e8d1c4ccbcdcba8c6
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-07-17 12:29:45 +0800

    Update data_preprocess.py

--- saprot/utils/data_preprocess.py
+++ saprot/utils/data_preprocess.py
@@ -10,7 +10,13 @@ from string import ascii_uppercase, ascii_lowercase
 from immutabledict import immutabledict
 from dataclasses import dataclass
 
-from saprot.utils.foldseek_util import StructuralAwareSequence, get_struc_seq, StructuralAwareSequences, Mask,FoldSeek
+from saprot.utils.foldseek_util import (
+    StructuralAwareSequence,
+    get_struc_seq,
+    StructuralAwareSequences,
+    Mask,
+    FoldSeek,
+)
 
 from saprot.utils.mpr import MultipleProcessRunnerSimplifier
 
@@ -57,13 +63,21 @@ DATA_TYPES_HINT = Literal[
     "Multiple_pairs_of_PDB/CIF_Structures",
 ]
 
+
 @dataclass
 class UniProtID:
     uniprot_id: str
-    uniprot_type: Literal['AF2']
+    uniprot_type: Literal["AF2", "PDB"]
     chain_id: str
 
-    SA_seq: StructuralAwareSequences=None
+    SA_seq: StructuralAwareSequences = None
+
+    @property
+    def __post_init__(self):
+        if self.uniprot_type not in ["AF2", "PDB"]:
+            raise ValueError(
+                f"UniProt type must be either 'AF2' or 'PDB', got {self.uniprot_type}"
+            )
 
     @property
     def SA(self) -> StructuralAwareSequence:
@@ -75,31 +89,32 @@ class UniProtIDs:
     uniprot_ids: Union[Tuple[UniProtID]]
 
     def __post_init__(self):
-        if isinstance(self.uniprot_ids,UniProtID):
+        if isinstance(self.uniprot_ids, UniProtID):
             self.uniprot_ids = [self.uniprot_ids]
-    
+
     @property
     def all_labels(self) -> Tuple[str]:
         return tuple(uniprot.uniprot_id for uniprot in self.uniprot_ids)
-    
+
     @property
     def is_AF2_structures(self) -> bool:
-        return all(x.uniprot_type == 'AF2' for x in self.uniprot_ids)
-    
+        return all(x.uniprot_type == "AF2" for x in self.uniprot_ids)
 
-    def map_sa_to_uniprot_ids(self, sa_seqs:Tuple[StructuralAwareSequences]):
+    def map_sa_to_uniprot_ids(self, sa_seqs: Tuple[StructuralAwareSequences]):
 
         if not len(sa_seqs) == len(self.uniprot_ids):
-            raise ValueError(f"The number of uniprot ids ({len(self.uniprot_ids)}) and the number of sa_seqs ({len(sa_seqs)}) must be equal.")
+            raise ValueError(
+                f"The number of uniprot ids ({len(self.uniprot_ids)}) and the number of sa_seqs ({len(sa_seqs)}) must be equal."
+            )
 
         for i, sa in enumerate(sa_seqs):
             self.uniprot_ids[i].SA_seq = sa
         return
-    
+
     @property
     def SA_seqs_as_tuple(self) -> Tuple[StructuralAwareSequence]:
         return tuple(x.SA for x in self.uniprot_ids)
-    
+
 
 @dataclass
 class InputDataDispatcher:
@@ -109,124 +124,131 @@ class InputDataDispatcher:
     LMDB_HOME: str
     STRUCTURE_HOME: str
     FOLDSEEK_PATH: str
-    nproc: int= os.cpu_count()
-    
-    
-    def UniProtID2SA(self, proteins: Union[List[UniProtID], Tuple[UniProtID], UniProtID]) -> UniProtIDs:
-        protein_list = UniProtIDs(proteins)
-        files=self.uniprot2pdb(protein_list.uniprot_ids)
+    nproc: int = os.cpu_count()
 
-        foldseeq_runner=FoldSeek(self.FOLDSEEK_PATH, plddt_mask=protein_list.is_AF2_structures,nproc=self.nproc)
-        sas=foldseeq_runner.parallel_queries(files)
+    def UniProtID2SA(
+        self, proteins: Union[List[UniProtID], Tuple[UniProtID], UniProtID]
+    ) -> UniProtIDs:
+        protein_list = UniProtIDs(proteins)
+        files = self.uniprot2pdb(protein_list.uniprot_ids)
+
+        foldseeq_runner = FoldSeek(
+            self.FOLDSEEK_PATH,
+            plddt_mask=protein_list.is_AF2_structures,
+            nproc=self.nproc,
+        )
+        sas = foldseeq_runner.parallel_queries(files)
         protein_list.map_sa_to_uniprot_ids(sas)
 
         return protein_list
 
-    def parse_data(self, data_type: DATA_TYPES_HINT, raw_data: Union[str, Tuple, List]) -> Tuple[StructuralAwareSequence]:
+    def parse_data(
+        self, data_type: DATA_TYPES_HINT, raw_data: Union[str, Tuple, List]
+    ) -> Tuple[StructuralAwareSequence]:
 
         # 0. Single AA Sequence
-        if data_type == 'Single_AA_Sequence':
+        if data_type == "Single_AA_Sequence":
             input_seq: str = raw_data
 
             aa_seq = input_seq
 
-            return (StructuralAwareSequence(amino_acid_seq=aa_seq, structural_seq='#'*len(aa_seq)),)
+            return (
+                StructuralAwareSequence(
+                    amino_acid_seq=aa_seq, structural_seq="#" * len(aa_seq)
+                ),
+            )
 
         # 1. Single SA Sequence
-        if data_type == 'Single_SA_Sequence':
+        if data_type == "Single_SA_Sequence":
             input_seq = raw_data
             sa_seq = input_seq
 
-            return (StructuralAwareSequence(None,None).from_SA_sequence(sa_seq),)
+            return (StructuralAwareSequence(None, None).from_SA_sequence(sa_seq),)
 
         # 2. Single UniProt ID
-        if data_type == 'Single_UniProt_ID':
+        if data_type == "Single_UniProt_ID":
             input_seq = raw_data
             uniprot_id = input_seq
 
-            protein=UniProtID(uniprot_id,'AF2','A')
-            protein_list=self.UniProtID2SA(proteins=protein)
+            protein = UniProtID(uniprot_id, "AF2", "A")
+            protein_list = self.UniProtID2SA(proteins=protein)
 
             return protein_list.SA_seqs_as_tuple
 
         # 3. Single PDB/CIF Structure
-        if data_type == 'Single_PDB/CIF_Structure':
-            uniprot_id,struc_type,chain = raw_data[:3]
+        if data_type == "Single_PDB/CIF_Structure":
+            uniprot_id, struc_type, chain = raw_data[:3]
 
-            protein=UniProtID(uniprot_id,struc_type,chain)
-            protein_list=self.UniProtID2SA(protein)
+            protein = UniProtID(uniprot_id, struc_type, chain)
+            protein_list = self.UniProtID2SA(protein)
 
             return protein_list.SA_seqs_as_tuple
 
         # Multiple sequences
         # raw_data = upload_files/xxx.csv
-        if data_type.startswith('Multiple'):
+        if data_type.startswith("Multiple"):
             uploaded_csv_path = raw_data
             csv_dataset_path = self.DATASET_HOME / uploaded_csv_path.name
 
         # 4. Multiple AA Sequences
-        if data_type == 'Multiple_AA_Sequences':
+        if data_type == "Multiple_AA_Sequences":
             protein_df = pd.read_csv(uploaded_csv_path)
 
-            SA:List[StructuralAwareSequence]=[]
+            SA: List[StructuralAwareSequence] = []
 
             for index, aa_seq in protein_df["Sequence"].items():
-                SA.append(StructuralAwareSequence(amino_acid_seq=aa_seq, structural_seq='#'*len(aa_seq)))
+                SA.append(
+                    StructuralAwareSequence(
+                        amino_acid_seq=aa_seq, structural_seq="#" * len(aa_seq)
+                    )
+                )
 
             return tuple(SA)
 
         # 5. Multiple SA Sequences
-        if data_type == 'Multiple_SA_Sequences':
+        if data_type == "Multiple_SA_Sequences":
             protein_df = pd.read_csv(uploaded_csv_path)
 
-            SA:List[StructuralAwareSequence]=[]
+            SA: List[StructuralAwareSequence] = []
 
             for index, sa_seq in protein_df["Sequence"].items():
-                SA.append(StructuralAwareSequence(None,None).from_SA_sequence(sa_seq))
+                SA.append(StructuralAwareSequence(None, None).from_SA_sequence(sa_seq))
 
             return tuple(SA)
 
         # 6. Multiple UniProt IDs
-        if data_type == 'Multiple_UniProt_IDs':
+        if data_type == "Multiple_UniProt_IDs":
             protein_df = pd.read_csv(uploaded_csv_path)
-            protein_list = protein_df.iloc[:, 0].tolist()
-            self.uniprot2pdb(protein_list)
-            protein_list = [(uniprot_id, "AF2", "A") for uniprot_id in protein_list]
-            mprs = MultipleProcessRunnerSimplifier(
-                protein_list, pdb2sequence, n_process=2, return_results=True
-            )
-            outputs = mprs.run()
 
-            protein_df["Sequence"] = [output.split("\t")[1] for output in outputs]
-            protein_df.to_csv(csv_dataset_path, index=None)
-            return csv_dataset_path
+            protein_ids = protein_df.iloc[:, 0].tolist()
+            protein_list = self.UniProtID2SA(
+                [UniProtID(_id, "AF2", "A") for _id in protein_ids]
+            )
+
+            return protein_list.SA_seqs_as_tuple
 
         # 7. Multiple PDB/CIF Structures
-        if data_type == 'Multiple_PDB/CIF_Structures':
+        if data_type == "Multiple_PDB/CIF_Structures":
             protein_df = pd.read_csv(uploaded_csv_path)
-            # protein_list = [(uniprot_id, type, chain), ...]
-            # protein_list = [item.split('.')[0] for item in protein_df.iloc[:, 0].tolist()]
-            # self.uniprot2pdb(protein_list)
-            protein_list = []
-            for row_tuple in protein_df.itertuples(index=False):
-                assert row_tuple.type in [
-                    "PDB",
-                    "AF2",
-                ], 'The type of structure must be either "PDB" or "AF2"!'
-                protein_list.append(row_tuple)
-            mprs = MultipleProcessRunnerSimplifier(
-                protein_list, pdb2sequence, n_process=2, return_results=True
-            )
-            outputs = mprs.run()
 
-            protein_df["Sequence"] = [output.split("\t")[1] for output in outputs]
-            protein_df.to_csv(csv_dataset_path, index=None)
-            return csv_dataset_path
+            id_col = protein_df["Sequence"].to_list()
+            type_col = protein_df["type"].to_list()
+            chain_col = protein_df["chain"].to_list()
+
+            protein_list = self.UniProtID2SA(
+                [
+                    UniProtID(_id, _type, _chain)
+                    for _id, _type, _chain in zip(id_col, type_col, chain_col)
+                ]
+            )
+
+            return protein_list.SA_seqs_as_tuple
 
         # 8. SaprotHub Dataset
-        elif data_type == 'SaprotHub_Dataset':
+        elif data_type == "SaprotHub_Dataset":
+            raise NotImplementedError
             input_repo_id = raw_data
-            REPO_ID = input_repo_id.value
+            REPO_ID = input_repo_id
 
             if REPO_ID.startswith("/"):
                 return Path(REPO_ID)
@@ -238,53 +260,55 @@ class InputDataDispatcher:
             return self.LMDB_HOME / REPO_ID
 
         # 9. Pair Single AA Sequences
-        elif data_type == 'A_pair_of_AA_Sequences':
-            input_seq_1, input_seq_2 = raw_data
-            sa_seq1 = get_SA_sequence_by_data_type('Single_AA_Sequence', input_seq_1)
-            sa_seq2 = get_SA_sequence_by_data_type('Single_AA_Sequence', input_seq_2)
+        elif data_type == "A_pair_of_AA_Sequences":
 
-            return (sa_seq1, sa_seq2)
+            return tuple(
+                StructuralAwareSequence(
+                    amino_acid_seq=aa_seq, structural_seq="#" * len(aa_seq)
+                )
+                for aa_seq in raw_data
+            )
 
         # 10. Pair Single SA Sequences
-        elif data_type == 'A_pair_of_SA_Sequences':
+        elif data_type == "A_pair_of_SA_Sequences":
             input_seq_1, input_seq_2 = raw_data
-            sa_seq1 = get_SA_sequence_by_data_type('Single_SA_Sequence', input_seq_1)
-            sa_seq2 = get_SA_sequence_by_data_type('Single_SA_Sequence', input_seq_2)
 
-            return (sa_seq1, sa_seq2)
+            return tuple(
+                StructuralAwareSequence(None, None).from_SA_sequence(sa_seq)
+                for sa_seq in raw_data
+            )
 
         # 11. Pair Single UniProt IDs
-        elif data_type == 'A_pair_of_UniProt_IDs':
-            input_seq_1, input_seq_2 = raw_data
-            sa_seq1 = get_SA_sequence_by_data_type('Single_UniProt_ID', input_seq_1)
-            sa_seq2 = get_SA_sequence_by_data_type('Single_UniProt_ID', input_seq_2)
+        elif data_type == "A_pair_of_UniProt_IDs":
 
-            return (sa_seq1, sa_seq2)
+            protein_list = self.UniProtID2SA(
+                proteins=[UniProtID(uniprot_id, "AF2", "A") for uniprot_id in raw_data]
+            )
+            return protein_list.SA_seqs_as_tuple
 
         # 12. Pair Single PDB/CIF Structure
-        if data_type == 'A_pair_of_PDB/CIF_Structures':
-            uniprot_id1 = raw_data[0]
-            struc_type1 = raw_data[1].value
-            chain1 = raw_data[2].value
+        if data_type == "A_pair_of_PDB/CIF_Structures":
 
-            protein_list1 = [(uniprot_id1, struc_type1, chain1)]
-            mprs1 = MultipleProcessRunnerSimplifier(
-                protein_list1, pdb2sequence, n_process=2, return_results=True
+            if raw_datalen := len(raw_data) % 3 != 0:
+                raise ValueError(
+                    "The length of the raw_data should be a multiple of 3."
+                )
+
+            uniprot_id_slice = slice(0, raw_datalen, 3)
+            struc_type_slice = slice(1, raw_datalen, 3)
+            chain_slice = slice(2, raw_datalen, 3)
+
+            protein_list = self.UniProtID2SA(
+                proteins=[
+                    UniProtID(_id, _type, _chain)
+                    for _id, _type, _chain in zip(
+                        raw_data[uniprot_id_slice],
+                        raw_data[struc_type_slice],
+                        raw_data[chain_slice],
+                    )
+                ]
             )
-            seqs1 = mprs1.run()
-            sa_seq1 = seqs1[0].split("\t")[1]
-
-            uniprot_id2 = raw_data[3]
-            struc_type2 = raw_data[4].value
-            chain2 = raw_data[5].value
-
-            protein_list2 = [(uniprot_id2, struc_type2, chain2)]
-            mprs2 = MultipleProcessRunnerSimplifier(
-                protein_list2, pdb2sequence, n_process=2, return_results=True
-            )
-            seqs2 = mprs2.run()
-            sa_seq2 = seqs2[0].split("\t")[1]
-            return sa_seq1, sa_seq2
+            return protein_list.SA_seqs_as_tuple
 
         # # Pair raw_data = upload_files/xxx.csv
         # if data_type in data_type_list[12:16]:
@@ -292,7 +316,7 @@ class InputDataDispatcher:
         #   csv_dataset_path = DATASET_HOME / uploaded_csv_path.name
 
         # 13. Pair Multiple AA Sequences
-        if data_type == 'Multiple_pairs_of_AA_Sequences':
+        if data_type == "Multiple_pairs_of_AA_Sequences":
             protein_df = pd.read_csv(uploaded_csv_path)
             for index, value in protein_df["seq_1"].items():
                 sa_seq1 = ""
@@ -316,7 +340,7 @@ class InputDataDispatcher:
             return csv_dataset_path
 
         # 14. Pair Multiple SA Sequences
-        if data_type == 'Multiple_pairs_of_SA_Sequences':
+        if data_type == "Multiple_pairs_of_SA_Sequences":
             protein_df = pd.read_csv(uploaded_csv_path)
 
             protein_df["name_1"] = "name_1"
@@ -329,13 +353,12 @@ class InputDataDispatcher:
             return csv_dataset_path
 
         # 15. Pair Multiple UniProt IDs
-        if data_type == 'Multiple_pairs_of_UniProt_IDs':
+        if data_type == "Multiple_pairs_of_UniProt_IDs":
             protein_df = pd.read_csv(uploaded_csv_path)
             protein_list1 = protein_df.loc[:, "seq_1"].tolist()
             self.uniprot2pdb(protein_list1)
             protein_df["name_1"] = protein_list1
             protein_list1 = [(uniprot_id, "AF2", "A") for uniprot_id in protein_list1]
-
 
             mprs1 = MultipleProcessRunnerSimplifier(
                 protein_list1, pdb2sequence, n_process=2, return_results=True
@@ -367,7 +390,7 @@ class InputDataDispatcher:
         #   print(Fore.BLUE+"Successfully upload your .csv file!"+Style.RESET_ALL)
         #   print("="*100)
 
-        elif data_type == 'Multiple_pairs_of_PDB/CIF_Structures':
+        elif data_type == "Multiple_pairs_of_PDB/CIF_Structures":
             protein_df = pd.read_csv(uploaded_csv_path)
             # columns: seq_1, seq_2, type_1, type_2, chain_1, chain_2, label, stage
 
@@ -401,28 +424,40 @@ class InputDataDispatcher:
             protein_df.to_csv(csv_dataset_path, index=None)
             return csv_dataset_path
 
-
     ################################################################################
     ########################## Download predicted structures #######################
     ################################################################################
-    def uniprot2pdb(self,uniprot_ids, nprocess=20) -> Tuple[str]:
+    def uniprot2pdb(self, uniprot_ids, nprocess=20) -> Tuple[str]:
         from saprot.utils.downloader import AlphaDBDownloader
 
         os.makedirs(self.STRUCTURE_HOME, exist_ok=True)
         # check exists files
-        exists_file=set([x for x in os.listdir(self.STRUCTURE_HOME) if x.endswith(".pdb") or x.endswith(".cif")])
+        exists_file = set(
+            [
+                x
+                for x in os.listdir(self.STRUCTURE_HOME)
+                if x.endswith(".pdb") or x.endswith(".cif")
+            ]
+        )
         if exists_file:
-            warnings.warn(f"{len(exists_file)} files already exists in {self.STRUCTURE_HOME}")
+            warnings.warn(
+                f"{len(exists_file)} files already exists in {self.STRUCTURE_HOME}"
+            )
 
         af2_downloader = AlphaDBDownloader(
             uniprot_ids, "pdb", save_dir=self.STRUCTURE_HOME, n_process=nprocess
         )
         af2_downloader.run()
 
-        updated_files = set([x for x in os.listdir(self.STRUCTURE_HOME) if x.endswith(".pdb") or x.endswith(".cif")])
-        
-        return Tuple(updated_files.difference(exists_file))
+        updated_files = set(
+            [
+                x
+                for x in os.listdir(self.STRUCTURE_HOME)
+                if x.endswith(".pdb") or x.endswith(".cif")
+            ]
+        )
 
+        return Tuple(updated_files.difference(exists_file))
 
     ################################################################################
     ############### Form foldseek sequences by multiple processes ##################
